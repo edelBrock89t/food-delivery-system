016eb30e68577c1cf45355bee5373a8eaa02442c
edelBrock18t <williamedelbrock@gmail.com>
2026-06-02 23:37:35 +0200

    feat: creating new database entity - PaymentEntity. Adding additional hibernate property in application.properties

---
+++ payment-service/src/main/java/com/food_delivery_system/payment_service/entity/payment/PaymentEntity.java
@@ -0,0 +1,37 @@
+package com.food_delivery_system.payment_service.entity.payment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payments")
+public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false, unique = true)
+    private Long orderId;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status", nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_method", nullable = false)
+    private PaymentMethod paymentMethod;
+}
